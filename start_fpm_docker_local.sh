93a96a892e228ce3be902253e7ecb9841944bedb
wanjochan <wanjochan@wanjochans-MacBook-Pro.local>
2016-07-25 14:35:03 +0800

    start_fpm_docker_local

--- start_fpm_docker_local.sh
+++ start_fpm_docker_local.sh
@@ -1,15 +1,10 @@
-echo TODO this is test only, the start/stop/reload is not yet sync from ...you know...
-
 dt=`date +%Y%m%d%H%M%S`
 echo $dt
 
 dd=$(cd `dirname $0`; pwd)
 echo ${dd}/webroot
 
-# NOTES:
-# -p => the prefix, see php-fpm conf
 php-fpm -d STARTDT=$dt -y php-fpm-docker-local.conf -p /app_root/webroot -F &
-#php-fpm -y php-fpm.conf -p . -F
 
 sleep 2
 ps aux |grep $dt
