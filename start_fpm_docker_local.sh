d227763f584e14af2644e6bba4caf4c632aff502
Wanjo Chan <wanjochan@gmail.com>
2016-07-31 23:17:37 +0700

    fix the script in docker

--- start_fpm_docker_local.sh
+++ start_fpm_docker_local.sh
@@ -2,9 +2,9 @@ dt=`date +%Y%m%d%H%M%S`
 echo $dt
 
 dd=$(cd `dirname $0`; pwd)
-echo ${dd}/webroot
+echo ${dd}
 
-php-fpm -d STARTDT=$dt -y php-fpm-docker-local.conf -p /app_root/webroot -F &
+php-fpm -d fpmdt=$dt -y php-fpm-docker-local.conf -p /app_root/ -F &
 
 sleep 2
 ps aux |grep $dt
