f90f083e67089c64e0945d2dca04f2254e36cdb5
WANJO CHAN <wanjochan@users.noreply.github.com>
2016-08-20 10:17:15 +0800

    Update start_cmp_server_docker_local.sh

--- start_cmp_server_docker_local.sh
+++ start_cmp_server_docker_local.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-# TODO exist if not in docker
-
-echo start php-fpm locally on port 9000 in docker
+echo start php-fpm locally on port 9000 inside docker
 sh start_fpm_docker_local.sh &
 
+#wait for the fpm boot-up
 sleep 2
 
 ps aux |grep php
 
-cd /app_root/
-php cmp_app_server_docker.php 
+#run the server
+cd /app_root/ && php cmp_app_server_docker.php 
