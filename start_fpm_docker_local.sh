b61819e75235773d13ef0d238d4b66a3d158240a
WANJO CHAN <wanjochan@users.noreply.github.com>
2016-08-20 10:18:13 +0800

    Update start_fpm_docker_local.sh

--- start_fpm_docker_local.sh
+++ start_fpm_docker_local.sh
@@ -1,8 +1,8 @@
 dt=`date +%Y%m%d%H%M%S`
-echo $dt
+#echo $dt
 
-dd=$(cd `dirname $0`; pwd)
-echo ${dd}
+#dd=$(cd `dirname $0`; pwd)
+#echo ${dd}
 
 php-fpm -d fpmdt=$dt -y php-fpm-docker-local.conf -p /app_root/webroot -F &
 
